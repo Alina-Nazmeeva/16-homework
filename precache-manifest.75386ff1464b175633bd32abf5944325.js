50aefff498072ffb88d4207365d710290fce628e
Alina Nazmeeva <ell122zmey@gmail.com>
2020-09-12 03:28:32 +0300

    Updates

---
+++ precache-manifest.75386ff1464b175633bd32abf5944325.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e5de438209ff0ed9f8d5d546576ce9",
+    "url": "/16-homework/index.html"
+  },
+  {
+    "revision": "2098ae02a6ec7c405423",
+    "url": "/16-homework/static/css/main.edd2b94f.chunk.css"
+  },
+  {
+    "revision": "840f34feba3cbdb414eb",
+    "url": "/16-homework/static/js/2.487864ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/16-homework/static/js/2.487864ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2098ae02a6ec7c405423",
+    "url": "/16-homework/static/js/main.86b64f9c.chunk.js"
+  },
+  {
+    "revision": "9a926c73a3679ad135c8",
+    "url": "/16-homework/static/js/runtime-main.1a516c65.js"
+  },
+  {
+    "revision": "a8023cdfb423171bdbb54a6ae6157a15",
+    "url": "/16-homework/static/media/female.a8023cdf.png"
+  },
+  {
+    "revision": "6d61aca982b1be00bc40091c7e311083",
+    "url": "/16-homework/static/media/male.6d61aca9.png"
+  }
+]);
